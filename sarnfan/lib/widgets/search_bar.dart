ee9e54f00217ca1cbaa657179aadeb519e8a730a
minklim <minklim47@gmail.com>
2024-06-23 12:40:36 +0700

    [edit] manage search

--- sarnfan/lib/widgets/search_bar.dart
+++ sarnfan/lib/widgets/search_bar.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:sarnfan/themes/color_theme.dart';
 
 class SearchField extends StatefulWidget {
-  const SearchField({super.key});
+  final TextEditingController controller;
+
+  final Function(String) searchKeyword;
+  const SearchField(
+      {super.key, required this.controller, required this.searchKeyword});
 
   @override
   State<SearchField> createState() => _SearchFieldState();
@@ -25,11 +29,16 @@ class _SearchFieldState extends State<SearchField> {
         TextFormField(
           maxLines: 1,
           scrollPadding: const EdgeInsets.all(20),
+          controller: widget.controller,
           decoration: InputDecoration(
               hintText: "e.g. post title, content, etc.",
               suffixIcon: Padding(
                 padding: const EdgeInsets.only(right: 10),
-                child: IconButton(icon: const Icon(Icons.search), onPressed: () {}),
+                child: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () async {
+                      await widget.searchKeyword(widget.controller.text);
+                    }),
               )),
         )
       ],
